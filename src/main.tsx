2c047de5a241b5b7b2efe79ed806f4eb79520e2e
frosstbiite <andrewmars24@gmail.com>
2026-08-22 12:31:33 -0600

    Render v1.1.1 extra Driver widgets with main overlay

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,29 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import ExtraDriverWidgets from './ExtraDriverWidgets.tsx'
 import RaceTicker from './RaceTicker.tsx'
 import ControlPanel from './ControlPanel.tsx'
 import './v111.css'
+
 const parameters = new URLSearchParams(window.location.search)
 const selectedView = parameters.get('view')
-let Overlay = App
+
+function DriverOverlay() {
+  return (
+    <>
+      <App />
+      <ExtraDriverWidgets />
+    </>
+  )
+}
+
+let Overlay = DriverOverlay
 if (selectedView === 'ticker') Overlay = RaceTicker
 if (selectedView === 'control') Overlay = ControlPanel
-createRoot(document.getElementById('root')!).render(<StrictMode><Overlay /></StrictMode>)
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Overlay />
+  </StrictMode>,
+)
